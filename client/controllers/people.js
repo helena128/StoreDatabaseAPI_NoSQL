d8108c53891312c17280923796e199762e7a9019
Elena Cheprasova <hellogbel@gmail.com>
2017-11-09 12:28:23 +0300

    deleting people implemented

--- client/controllers/people.js
+++ client/controllers/people.js
@@ -12,19 +12,37 @@ myApp.controller('PeopleController', [ '$scope', '$http', '$location', '$routePa
             $scope.people = data;
             console.log(response.data);
         })*/
+        var refresh = function() {
+            $http.get('/api/people').then(function(response){
+                var data = response.data;
+                $scope.people = data;
+                console.log(data);
+            });
+        };
+
         $scope.getPeople = function () {
             $http.get('/api/people').then(function(response){
                 var data = response.data;
                 $scope.people = data;
                 console.log(data);
             });
-        }
+        };
 
-        $scope.getHuman = function () {
+        $scope.getHuman = function (id) {
             $http.get('/api/people/:id').then(function(response){
                 var data = response.data;
                 $scope.human = data;
                 console.log(data);
             });
+        };
+
+        $scope.removePeople = function (id) {
+            $http.delete('/api/people/' + id).then(function(response) {
+                //refresh();
+                console.log('Book deleted');
+                window.location.href="#/people";
+            }).catch (function (){
+                console.log('Error while deleting');
+            })
         }
 }]);
